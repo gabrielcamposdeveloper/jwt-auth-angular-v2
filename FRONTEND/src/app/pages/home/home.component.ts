29dd7d8de281da6c4a3b997929efd3a560db7707
Gabriel Campos <gabrielcampos.dev@outlook.com>
2025-05-29 14:13:54 -0300

    [Update] Gráficos do Dashboard mudam confor escolhido o tipo do gráfico

--- FRONTEND/src/app/pages/home/home.component.ts
+++ FRONTEND/src/app/pages/home/home.component.ts
@@ -2,13 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: '<p-chart type="bar" [data]="chartData"></p-chart>'
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  tipoGrafico = 'bar';
+  mostrarGrafico = true;
+
+  tipos = ['bar', 'line', 'radar', 'doughnut', 'pie'];
+
   chartData = {
     labels: ['Usuário A', 'Usuário B', 'Usuário C'],
     datasets: [
-      { label: 'Acessos', data: [5, 10, 7] }
+      {
+        label: 'Acessos',
+        data: [5, 10, 7],
+        backgroundColor: ['#42A5F5', '#66BB6A', '#FFA726']
+      }
     ]
   };
+
+  chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  recriarGrafico() {
+    this.mostrarGrafico = false;
+    setTimeout(() => {
+      this.mostrarGrafico = true;
+    }, 0);
+  }
 }
