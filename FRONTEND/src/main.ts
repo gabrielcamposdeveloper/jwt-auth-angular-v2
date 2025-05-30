680eb649b77d5c0cb674e0cf8f650af3235be2cd
Gabriel Campos <gabrielcampos.dev@outlook.com>
2025-05-29 23:20:36 -0300

    [Update] Limpeza arquivo

--- FRONTEND/src/main.ts
+++ FRONTEND/src/main.ts
@@ -1,23 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-import { Chart, BarController, LineController, RadarController, DoughnutController, PieController, CategoryScale, LinearScale, BarElement, PointElement, LineElement, ArcElement, Tooltip, Legend } from 'chart.js';
-
-Chart.register(
-  BarController,
-  LineController,
-  RadarController,
-  DoughnutController,
-  PieController,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  ArcElement,
-  Tooltip,
-  Legend
-);
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
